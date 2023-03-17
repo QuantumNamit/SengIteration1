902a5c575474996deabd067679e700b43b3139d2
Namit Aneja <namit.aneja@ucalgary.ca>
2023-03-16 21:34:14 -0600

    update attendantObserver.java

--- com.autovend.software/src/com/autovend/software/AttendantObserver.java
+++ com.autovend.software/src/com/autovend/software/AttendantObserver.java
@@ -1,10 +1,10 @@
 package com.autovend.software;
-
 import com.autovend.devices.OverloadException;
+import com.autovend.devices.ReceiptPrinter;
 
 public interface AttendantObserver {
 
-    public void notifyAttendant() throws OverloadException;
-    public void putPaper() throws OverloadException;
-    public void putInk() throws OverloadException;
+    public void notifyAttendant(ReceiptPrinter printer ,receiptPrinterController controller ,AttendantObserver observer,int paperRemaining ,int inkRemaining) throws OverloadException;
+    public void putPaper(ReceiptPrinter printer) throws OverloadException;
+    public void putInk(ReceiptPrinter printer) throws OverloadException;
 }
