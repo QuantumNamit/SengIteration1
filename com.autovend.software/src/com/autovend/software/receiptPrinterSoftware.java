007c69fde2e4a6aea06b5e8c160a0397f8a7578c
Namit Aneja <namit.aneja@ucalgary.ca>
2023-03-16 21:35:01 -0600

    updated receiptPrinterSoftware.java

--- com.autovend.software/src/com/autovend/software/receiptPrinterSoftware.java
+++ com.autovend.software/src/com/autovend/software/receiptPrinterSoftware.java
@@ -1,5 +1,6 @@
 package com.autovend.software;
 import com.autovend.*;
+import com.autovend.devices.EmptyException;
 import com.autovend.devices.OverloadException;
 import com.autovend.devices.ReceiptPrinter;
 import com.autovend.devices.SelfCheckoutStation;
@@ -7,24 +8,25 @@ import com.autovend.products.BarcodedProduct;
 
 import java.util.ArrayList;
 
-public class receiptPrinterSoftware  implements AttendantObserver,CustomerObserver{
+public class receiptPrinterSoftware implements AttendantObserver,CustomerObserver{
 
         private ArrayList<BarcodedProduct> items;
-        private receiptPrinterController controller;
         private ReceiptPrinter printer;
-        private int paperRemaining;
-        private int inkRemaining;
+
         private SelfCheckoutStationController slf;
-        private AttendantObserver observer;
         private int paper_units;
         private int ink_units;
 
+        //Constructor
         public receiptPrinterSoftware(ArrayList<BarcodedProduct> items){
             this.items = items;
         }
 
-        public void printReceipt(boolean paidInFull, BarcodedProduct product) {
+
+        // print receipt
+        public void printReceipt(SelfCheckoutStationController slf,ReceiptPrinter printer,receiptPrinterController controller,AttendantObserver observer ,boolean paidInFull,int paperRemaining,int inkRemaining) throws OverloadException, EmptyException {
             try {
+
                 if (paidInFull) {
                     for (BarcodedProduct unit : items) {
                         String itemString = "Item: " + unit.getDescription() + ", Weight: " + unit.getExpectedWeight()+ "lbs, Price: " + unit.getPrice() + "$\n";
@@ -32,44 +34,30 @@ public class receiptPrinterSoftware  implements AttendantObserver,CustomerObserv
                             printer.print(itemString.charAt(i));
                         }
                     }
-                    printer.cutPaper();
-                    notifyCustomerSessionComplete();
-                    notifyCustomer();
-                }
+                printer.cutPaper();
+                notifyCustomerSessionComplete();
+                notifyCustomer();
+            }
 
                 // Check if printer has run out of paper or ink
                 if (controller.outOfPaper(paperRemaining) || controller.outOfInk(inkRemaining)) {
                     // Abort printing and suspending station
-                    controller.abortPrinting();
-                    notifyAttendant();
+                    controller.abortPrinting(slf);
+                    notifyAttendant(printer,controller,observer,paperRemaining,inkRemaining);
                 }
             } catch (Exception e) {
                 // Handle printer exception
-                System.out.println("Error while printing receipt: " + e.getMessage());
+             System.out.println("Error while printing receipt: " + e.getMessage());
             }
         }
 
     @Override
-    public void notifyAttendant() throws OverloadException {
-        // Inform attendant to print duplicate receipt and perform maintenance
-        System.out.println("Receipt printer ran out of paper or ink. Please print a duplicate receipt and perform maintenance on the station.");
-        if(controller.outOfPaper(paperRemaining)){
-
-            observer.putPaper();
-        }
-        else if (controller.outOfInk(inkRemaining)){
-            observer.putInk();
-        }
-
-    }
-
-    @Override
-    public void putPaper() throws OverloadException {
+    public void putPaper(ReceiptPrinter printer) throws OverloadException {
         printer.addPaper(paper_units);
     }
 
     @Override
-    public void putInk() throws OverloadException {
+    public void putInk(ReceiptPrinter printer) throws OverloadException {
         printer.addInk(ink_units);
     }
 
@@ -79,8 +67,23 @@ public class receiptPrinterSoftware  implements AttendantObserver,CustomerObserv
     }
 
     @Override
-    public void notifyCustomerSessionComplete() {
+    public void notifyCustomerSessionComplete(){
+        System.out.println("Session Complete ");
+    }
+
+    @Override
+    public void notifyAttendant(ReceiptPrinter printer,receiptPrinterController controller,AttendantObserver observer,int paperRemaining ,int inkRemaining) throws OverloadException {
+        // Inform attendant to print duplicate receipt and perform maintenance
+        System.out.println("Receipt printer ran out of paper or ink. Please print a duplicate receipt and perform maintenance on the station.");
+        if(controller.outOfPaper(paperRemaining)){
+
+            observer.putPaper(printer);
+        }
+        else if (controller.outOfInk(inkRemaining)){
+            observer.putInk(printer);
+        }
 
     }
+
 }
 
