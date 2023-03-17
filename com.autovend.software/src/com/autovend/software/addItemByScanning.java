e244847f17a149308d52a17e367f60d5186d0e7d
namitaneja123 <115895907+namitaneja123@users.noreply.github.com>
2023-03-17 15:10:00 -0600

    Update addItemByScanning.java
    Updated the addItemByScanning code file

--- com.autovend.software/src/com/autovend/software/addItemByScanning.java
+++ com.autovend.software/src/com/autovend/software/addItemByScanning.java
@@ -1,77 +1,128 @@
 package com.autovend.software;
 import com.autovend.Barcode;
 import com.autovend.BarcodedUnit;
-import com.autovend.SellableUnit;
 import com.autovend.devices.BarcodeScanner;
-import com.autovend.devices.BillSlot;
 import com.autovend.devices.ElectronicScale;
-import com.autovend.devices.SelfCheckoutStation;
 import com.autovend.devices.SimulationException;
 import com.autovend.products.BarcodedProduct;
-
 import java.math.BigDecimal;
 
 
-public class addItemByScanning  extends addItem implements CustomerObserver {
+/**
+ *                         Scenarios to Focus On
+ *
+ *    1. Laser Scanner: Detects a barcode and signals this to the System.
+ *    2. System: Blocks the self-checkout station from further customer interaction.
+ *    3. System: Determines the characteristics (weight and cost) of the product associated with the barcode.
+ *    4. System:Updates the expected weight from the BaggingArea.
+ *    5. System: Signals to the Customer I/O to place the scanned item in the Bagging Area.
+ *    6. Bagging Area: Signals to the System that the weight has changed.
+ *    7. System:Unblocks the station.
+ *
+ */
 
 
-    private SelfCheckoutStationController slf;
+public class addItemByScanning  implements CustomerObserver {
+
     private Barcode barcode;
     private String description;
     private double weight;
     private BigDecimal cost;
-    BarcodedUnit item;
+    private BarcodedUnit item;
+    private BigDecimal price;
+    private Boolean isPerUnit;
+    private double expectedWeight;
 
+    /**
+     * Constructor
+     * @param item - This is the item which the Customer is scanning
+     * @param product-The item with the Barcode.
+     */
     public addItemByScanning(BarcodedUnit item, BarcodedProduct product){
 
-        // all the general information about the item
+        // if item trying to add is null
+        if (item == null)
+            throw new SimulationException(new NullPointerException("item"));
 
-        super(item, product);
-
-        // information about the item if adding the item by scanning the barcode
-
-            this.barcode=product.getBarcode();
-            this.description= product.getDescription();
+        //  information about the item
+        this.item=item;
+        this.price=product.getPrice();
+        this.weight=item.getWeight();
+        this.isPerUnit=product.isPerUnit();
+        this.expectedWeight=product.getExpectedWeight();
+        this.barcode=product.getBarcode();
+        this.description= product.getDescription();
 
     }
 
+    /**
+     *
+     * @param item - the item which we are going to scan
+     * @param product- the item with the barcode
+     * @param controller - system which is handling everything
+     * @throws Exception- throws exception if the expected weight change differs the bagging area weight change
+     *
+     * Also if we try to put Product with null barcode it will throw Simulation Exception as covered inside the BarcodedProduct class
+     */
+   public void scanItem( BarcodedUnit item, BarcodedProduct product, SystemController controller) throws Exception {
 
-   public void scanItem(BarcodeScanner barcodeScanner, BarcodedUnit item, BarcodedProduct product, SelfCheckoutStationController slf) throws Exception {
+        // creating a barcodeScanner object to scan the items
+        BarcodeScanner barcodeScanner = controller.mainScanner;
 
-       // laser scanner scans the items and if successful it will notify all the Barcode Scanner observers
-       if(barcodeScanner.scan(item)){
 
-            //  should block out the self checkout station form further customer interaction
-           block_selfcheckout(slf);
+        // barcodeScanner scans the items and if successful it will notify all the Barcode Scanner observers (Scenario 1 covered)
 
-           weight= item.getWeight();
-           cost=product.getPrice();
+        if(barcodeScanner.scan(item)){
 
-           expectedWeightUpdate(slf.baggingArea,product);
+            // should block out the System from further customer interaction (Scenario 2 covered)
 
-           // notify customer to place the item
+               controller.disable(controller);
 
-           notifyCustomer();
+            // System determining the characteristics such as weight and cost of the product  (Scenario 3 covered)
 
-       //    bagging_area_weight_change(scale,product.getBarcode(),itemWeight);
-           if(expectedWeightUpdate(slf.baggingArea,product)!= bagging_area_weight_change(slf.baggingArea,product.getBarcode(), item.getWeight())){
-              throw new Exception("Weight Discrepancy Occurred");
+               weight= controller.getWeight(item);
+               cost=controller.getPrice(product);
+
+             // notifying the customer to place the item in the bagging Area (Scenario 5 covered)
+
+                notifyCustomer();
+
+             // updates the expected weight from the bagging area (covered Scenario 4)
+
+                double expected_change=controller.expectedWeightUpdate(product);
+                double bagging_weight_change= controller.bagging_area_weight_change(controller,product.getBarcode(),weight);
+
+
+              // If the expected change differs the bagging area weight change throwing a Weight Discrepancy Error (Exception covered)
+
+                if(bagging_weight_change!=expected_change){
+                          throw new Exception("Weight Discrepancy Occurred");
            }
 
-           // unblock selfcheckout station
-           unblock_selfcheckout(slf);
-          
-       }
+              // Otherwise adding the item to the bagging area and if the addition is successful then Electronic Scale Observer will be notified by the Hardware code( covered Scenario 6)
+               ElectronicScale baggingArea = controller.baggingArea;
+               baggingArea.add(item);
 
-  }
+               // unblock the System (covered scenario 7)
+                controller.enable(controller);
+
+           }
+           }
+
+    /**
+     * Method to notify the customer to place there item to the bagging area
+     */
 
     @Override
     public void notifyCustomer() {
         System.out.println("Enter the item to the Bagging Area ");
     }
 
+
     @Override
     public void notifyCustomerSessionComplete() {
 
+
     }
 }
+
