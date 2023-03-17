a580310bc4bd3ae2310c82aa8f3ab3bf1cbffeda
namitaneja123 <115895907+namitaneja123@users.noreply.github.com>
2023-03-17 15:17:09 -0600

    Update addItemByScanningTest.java
    made changes in addItemByScanning Test file

--- com.autovend.software.test/src/com/autovend/software/test/addItemByScanningTest.java
+++ com.autovend.software.test/src/com/autovend/software/test/addItemByScanningTest.java
@@ -2,37 +2,32 @@ package com.autovend.software.test;
 import com.autovend.Barcode;
 import com.autovend.BarcodedUnit;
 import com.autovend.Numeral;
-import com.autovend.SellableUnit;
-import com.autovend.devices.BarcodeScanner;
-import com.autovend.devices.ElectronicScale;
 import com.autovend.devices.OverloadException;
 import com.autovend.devices.SimulationException;
 import com.autovend.products.BarcodedProduct;
-import com.autovend.software.SelfCheckoutStationController;
-import com.autovend.software.addItemByScanning;
-
 import org.junit.Test;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Currency;
 
 import static org.junit.Assert.*;
 
 public class addItemByScanningTest {
 
+    /**
+     * This is the test to  check whether scan function worked successfully or not
+     *
+     */
     @Test
     public void scanItem_checking_successful_scan() throws Exception {
 
-
-
+        // Initialising objects
         Barcode barcode =new Barcode(Numeral.five);
         BarcodedUnit unit=new BarcodedUnit(barcode,1.2);
         String description ="item";
         BigDecimal price =new BigDecimal(2);
         double expectedWeight =1.2;
 
-        BarcodeScanner scanner =new BarcodeScanner();
         BarcodedProduct product =new BarcodedProduct(barcode,description,price,expectedWeight);
         addItemByScanning item=new addItemByScanning(unit, product);
 
@@ -43,23 +38,29 @@ public class addItemByScanningTest {
         BigDecimal[] coindeno=new BigDecimal[2];
         coindeno[0]=a;
         coindeno[1]=b;
-        SelfCheckoutStationController slf=new SelfCheckoutStationController(currency,deno,coindeno,12,2);
+        SystemController slf=new SystemController(currency,deno,coindeno,12,2);
 
-        item.scanItem(scanner,unit,product,slf);
+
+        item.scanItem(unit,product,slf);
+        assertEquals(slf.expectedWeightUpdate(product),slf.bagging_area_weight_change(slf,barcode,1.2),0.00001);
 
     }
 
+
+    /**
+     *
+     * This is the test to check weight discrepancy
+     */
     @Test( expected=Exception.class)
     public void scanItem_checking_weight_discrepancy() throws Exception {
 
-
+        // Initialising objects
         Barcode barcode =new Barcode(Numeral.five);
         BarcodedUnit unit=new BarcodedUnit(barcode,1.2);
         String description ="item";
         BigDecimal price =new BigDecimal(2);
         double expectedWeight =2.0;
 
-        BarcodeScanner scanner =new BarcodeScanner();
         BarcodedProduct product =new BarcodedProduct(barcode,description,price,expectedWeight);
         addItemByScanning item=new addItemByScanning(unit, product);
 
@@ -70,11 +71,16 @@ public class addItemByScanningTest {
         BigDecimal[] coindeno=new BigDecimal[2];
         coindeno[0]=a;
         coindeno[1]=b;
-        SelfCheckoutStationController slf=new SelfCheckoutStationController(currency,deno,coindeno,3,3);
-        item.scanItem(scanner,unit,product,slf);
+
+        SystemController slf=new SystemController(currency,deno,coindeno,3,3);
+
+        item.scanItem(unit,product,slf);
 
     }
 
+    /**
+     * Test to check whether the simulation exception is thrown or not if we add a null unit
+     */
     @Test(expected = SimulationException.class)
     public void scanning_null_item(){
 
@@ -89,6 +95,24 @@ public class addItemByScanningTest {
 
     }
 
+
+    /**
+     * Test to check whether the simulation exception is thrown or not if we add a null barcode
+     */
+    @Test(expected =SimulationException.class)
+    public void scanItem_if_barcode_isNull() throws Exception {
+
+        // Initialising
+
+        Barcode barcode =null;
+        BarcodedUnit unit=new BarcodedUnit(barcode,1.2);
+
+    }
+
+
+    /**
+     * Test to check whether the Overload exception is thrown or not if we add a item with weight more than the weight limit
+     */
     @Test( expected= OverloadException.class)
     public void exceeding_weight_limit() throws Exception {
 
@@ -97,9 +121,8 @@ public class addItemByScanningTest {
         BarcodedUnit unit=new BarcodedUnit(barcode,3.3);
         String description ="item";
         BigDecimal price =new BigDecimal(2);
-        double expectedWeight =3.3;
+        double expectedWeight =3.4;
 
-        BarcodeScanner scanner =new BarcodeScanner();
         BarcodedProduct product =new BarcodedProduct(barcode,description,price,expectedWeight);
         addItemByScanning item=new addItemByScanning(unit, product);
 
@@ -110,11 +133,14 @@ public class addItemByScanningTest {
         BigDecimal[] coindeno=new BigDecimal[2];
         coindeno[0]=a;
         coindeno[1]=b;
-        SelfCheckoutStationController slf=new SelfCheckoutStationController(currency,deno,coindeno,2,1);
+        SystemController slf=new SystemController(currency,deno,coindeno,2,1);
 
 
-        item.scanItem(scanner,unit,product,slf);
+        item.scanItem(unit,product,slf);
 
     }
 
 }
+     
+    
+     
