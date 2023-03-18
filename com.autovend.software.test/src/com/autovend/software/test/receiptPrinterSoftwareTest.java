280c6286b38ab1a6cf9b6ef7618b417e9c9356e6
Namit Aneja <namit.aneja@ucalgary.ca>
2023-03-17 19:57:19 -0600

    added changes to receiptPrinterSoftwareTest.java

--- com.autovend.software.test/src/com/autovend/software/test/receiptPrinterSoftwareTest.java
+++ com.autovend.software.test/src/com/autovend/software/test/receiptPrinterSoftwareTest.java
@@ -5,7 +5,6 @@ import com.autovend.BarcodedUnit;
 import com.autovend.Numeral;
 import com.autovend.devices.EmptyException;
 import com.autovend.devices.OverloadException;
-import com.autovend.devices.ReceiptPrinter;
 import com.autovend.products.BarcodedProduct;
 import org.junit.Test;
 
@@ -15,7 +14,18 @@ import java.util.Currency;
 
 import static org.junit.Assert.*;
 
+/**
+ * Test File for receiptPrinterSoftware
+ */
+
 public class receiptPrinterSoftwareTest {
+
+
+    /**
+     *
+     * Test case to check the successful execution of the printReceipt function
+     * and when the customer paid the full price
+     */
 
     @Test
     public void printReceipt() throws EmptyException, OverloadException {
@@ -24,14 +34,13 @@ public class receiptPrinterSoftwareTest {
         BigDecimal price = new BigDecimal(5);
         double expected_weight=3;
         BarcodedProduct bp = new BarcodedProduct(barcode,description,price,expected_weight);
+
         ArrayList<BarcodedProduct> items=new ArrayList<BarcodedProduct>();
         items.add(bp);
-        ReceiptPrinter printer =new ReceiptPrinter();
-        printer.addInk(1<<20);
-        printer.addPaper(55);
+
 
         receiptPrinterSoftware rp =new receiptPrinterSoftware(items);
-        receiptPrinterController controller = new receiptPrinterController();
+
 
         BarcodedUnit unit=new BarcodedUnit(barcode,1.2);
         Currency currency = Currency.getInstance("USD");
@@ -40,31 +49,52 @@ public class receiptPrinterSoftwareTest {
         BigDecimal a =new BigDecimal(1);
         BigDecimal b =new BigDecimal(2);
         coindeno[0]=a;
+        coindeno[1]=b;
 
-        SystemController slf=new SystemController(currency,deno,coindeno,3,3);
+        SystemController controller=new SystemController(currency,deno,coindeno,3,3);
+        controller.addInk(controller,1<<20);
+        controller.addPaper(controller,22);
 
-
-        AttendantObserver observer = new AttendantObserver() {
-            @Override
-            public void notifyAttendant(ReceiptPrinter printer,receiptPrinterController controller, AttendantObserver observer, int paperRemaining, int inkRemaining) throws OverloadException {
-
-            }
-
-            @Override
-            public void putPaper(ReceiptPrinter printer) throws OverloadException {
-
-            }
-
-            @Override
-            public void putInk(ReceiptPrinter printer) throws OverloadException {
-
-            }
-        } ;
-
-        rp.printReceipt(slf ,printer,controller ,observer,true,55,1<<20);
+        rp.printReceipt(controller,true,55,1<<20);
 
     }
 
+    /**
+     *
+     * Test case to check the print receipt function if the customer did not paid the full price
+     */
+
+        @Test
+        public void  no_receipt_printed_not_paid_in_full () throws EmptyException, OverloadException {
+            Barcode barcode = new Barcode(Numeral.five);
+            String description ="Item";
+            BigDecimal price = new BigDecimal(5);
+            double expected_weight=3;
+            BarcodedProduct bp = new BarcodedProduct(barcode,description,price,expected_weight);
+            ArrayList<BarcodedProduct> items=new ArrayList<BarcodedProduct>();
+            items.add(bp);
+            receiptPrinterSoftware rp =new receiptPrinterSoftware(items);
+
+
+            BarcodedUnit unit=new BarcodedUnit(barcode,1.2);
+            Currency currency = Currency.getInstance("USD");
+            int [] deno = {1,2};
+            BigDecimal[] coindeno=new BigDecimal[2];
+            BigDecimal a =new BigDecimal(1);
+            BigDecimal b =new BigDecimal(2);
+            coindeno[0]=a;
+            coindeno[1]=b;
+            SystemController controller=new SystemController(currency,deno,coindeno,3,3);
+            controller.addInk(controller,1<<20);
+            controller.addPaper(controller,22);
+            rp.printReceipt(controller,false,55,1<<20);
+    }
+
+
+    /**
+     *
+     * Test case to check how the print receipt function works if we are out of paper
+     */
     @Test
     public void out_of_paper() throws EmptyException, OverloadException {
         Barcode barcode = new Barcode(Numeral.five);
@@ -74,13 +104,7 @@ public class receiptPrinterSoftwareTest {
         BarcodedProduct bp = new BarcodedProduct(barcode,description,price,expected_weight);
         ArrayList<BarcodedProduct> items=new ArrayList<BarcodedProduct>();
         items.add(bp);
-        ReceiptPrinter printer =new ReceiptPrinter();
-        printer.addPaper(55);
-        printer.addInk(1<<20);
-
         receiptPrinterSoftware rp =new receiptPrinterSoftware(items);
-        receiptPrinterController controller = new receiptPrinterController();
-
 
 
         Currency currency = Currency.getInstance("USD");
@@ -91,28 +115,17 @@ public class receiptPrinterSoftwareTest {
         coindeno[0]=a;
         coindeno[1]=b;
 
-        SystemController slf=new SystemController(currency,deno,coindeno,3,3);
+        SystemController controller=new SystemController(currency,deno,coindeno,3,3);
+        controller.addInk(controller,1<<20);
+        controller.addPaper(controller,22);
 
-        AttendantObserver observer = new AttendantObserver() {
-            @Override
-            public void notifyAttendant(ReceiptPrinter printer,receiptPrinterController controller, AttendantObserver observer, int paperRemaining, int inkRemaining) throws OverloadException {
-
-            }
-
-            @Override
-            public void putPaper(ReceiptPrinter printer) throws OverloadException {
-
-            }
-
-            @Override
-            public void putInk(ReceiptPrinter printer) throws OverloadException {
-
-            }
-        };
-        rp.printReceipt(slf,printer,controller,observer,true,0,1);
-
+        rp.printReceipt(controller,true,0,1);
     }
 
+    /**
+     *
+     * Test case to check how the print receipt function works if we are out of ink
+     */
     @Test
     public void out_of_ink() throws EmptyException, OverloadException {
         Barcode barcode = new Barcode(Numeral.five);
@@ -122,12 +135,68 @@ public class receiptPrinterSoftwareTest {
         BarcodedProduct bp = new BarcodedProduct(barcode,description,price,expected_weight);
         ArrayList<BarcodedProduct> items=new ArrayList<BarcodedProduct>();
         items.add(bp);
-        ReceiptPrinter printer =new ReceiptPrinter();
-        printer.addPaper(55);
-        printer.addInk(1<<20);
 
         receiptPrinterSoftware rp =new receiptPrinterSoftware(items);
-        receiptPrinterController controller = new receiptPrinterController();
+
+        Currency currency = Currency.getInstance("USD");
+        int [] deno = {1,2};
+        BigDecimal[] coindeno=new BigDecimal[2];
+        BigDecimal a =new BigDecimal(1);
+        BigDecimal b =new BigDecimal(2);
+        coindeno[0]=a;
+        coindeno[1]=b;
+        SystemController controller=new SystemController(currency,deno,coindeno,3,3);
+        controller.addInk(controller,1<<20);
+        controller.addPaper(controller,22);
+        rp.printReceipt(controller,true,1,0);
+
+    }
+
+    /**
+     *
+     * Test case to check how the print receipt function works if we are out of ink as well as out of paper
+     */
+
+    @Test
+    public void out_of_ink_and_out_of_paper() throws EmptyException, OverloadException {
+        Barcode barcode = new Barcode(Numeral.five);
+        String description ="Item";
+        BigDecimal price = new BigDecimal(5);
+        double expected_weight=3;
+        BarcodedProduct bp = new BarcodedProduct(barcode,description,price,expected_weight);
+        ArrayList<BarcodedProduct> items=new ArrayList<BarcodedProduct>();
+        items.add(bp);
+
+
+        receiptPrinterSoftware rp =new receiptPrinterSoftware(items);
+        Currency currency = Currency.getInstance("USD");
+        int [] deno = {1,2};
+        BigDecimal[] coindeno=new BigDecimal[2];
+        BigDecimal a =new BigDecimal(1);
+        BigDecimal b =new BigDecimal(2);
+        coindeno[0]=a;
+        coindeno[1]=b;
+        SystemController controller=new SystemController(currency,deno,coindeno,3,3);
+        controller.addInk(controller,1<<20);
+        controller.addPaper(controller,22);
+        rp.printReceipt(controller,true,0,0);
+    }
+
+    /**
+     *
+     * Test case to check whether the attendant has been notified or not when the printer got out of paper
+     */
+    @Test
+    public void notifyAttendant_out_of_paper() throws OverloadException, EmptyException {
+
+        Barcode barcode = new Barcode(Numeral.five);
+        String description ="Item";
+        BigDecimal price = new BigDecimal(5);
+        double expected_weight=3;
+        BarcodedProduct bp = new BarcodedProduct(barcode,description,price,expected_weight);
+        ArrayList<BarcodedProduct> items=new ArrayList<BarcodedProduct>();
+        items.add(bp);
+        receiptPrinterSoftware rp =new receiptPrinterSoftware(items);
 
 
         Currency currency = Currency.getInstance("USD");
@@ -138,30 +207,20 @@ public class receiptPrinterSoftwareTest {
         coindeno[0]=a;
         coindeno[1]=b;
 
-        SystemController slf=new SystemController(currency,deno,coindeno,3,3);
-
-        AttendantObserver observer = new AttendantObserver() {
-            @Override
-            public void notifyAttendant(ReceiptPrinter printer,receiptPrinterController controller, AttendantObserver observer, int paperRemaining, int inkRemaining) throws OverloadException {
-
-            }
-
-            @Override
-            public void putPaper(ReceiptPrinter printer) throws OverloadException {
-
-            }
-
-            @Override
-            public void putInk(ReceiptPrinter printer) throws OverloadException {
-
-            }
-        };
-        rp.printReceipt(slf,printer,controller,observer,true,1,0);
+        SystemController controller=new SystemController(currency,deno,coindeno,3,3);
+        controller.addInk(controller,1<<20);
+        controller.addPaper(controller,22);
+        rp.printReceipt(controller,true,1,0);
 
     }
-    @Test
-    public void notifyAttendant_out_of_paper() throws OverloadException {
 
+    /**
+     *
+     * Test case to check whether the attendant has been notified or not when the printer got out of  ink
+     */
+
+    @Test
+    public void notifyAttendant_out_of_ink() throws OverloadException, EmptyException {
         Barcode barcode = new Barcode(Numeral.five);
         String description ="Item";
         BigDecimal price = new BigDecimal(5);
@@ -169,105 +228,18 @@ public class receiptPrinterSoftwareTest {
         BarcodedProduct bp = new BarcodedProduct(barcode,description,price,expected_weight);
         ArrayList<BarcodedProduct> items=new ArrayList<BarcodedProduct>();
         items.add(bp);
-        ReceiptPrinter printer =new ReceiptPrinter();
-        printer.addPaper(55);
-        printer.addInk(1<<20);
-
         receiptPrinterSoftware rp =new receiptPrinterSoftware(items);
-        receiptPrinterController controller = new receiptPrinterController();
 
-        AttendantObserver observer =new AttendantObserver() {
-            @Override
-            public void notifyAttendant(ReceiptPrinter printer,receiptPrinterController controller, AttendantObserver observer, int paperRemaining, int inkRemaining) throws OverloadException {
-
-            }
-
-            @Override
-            public void putPaper(ReceiptPrinter printer) throws OverloadException {
-
-            }
-
-            @Override
-            public void putInk(ReceiptPrinter printer) throws OverloadException {
-
-            }
-        };
-
-        rp.notifyAttendant(printer,controller,observer,0,0);
-
+        Currency currency = Currency.getInstance("USD");
+        int [] deno = {1,2};
+        BigDecimal[] coindeno=new BigDecimal[2];
+        BigDecimal a =new BigDecimal(1);
+        BigDecimal b =new BigDecimal(2);
+        coindeno[0]=a;
+        coindeno[1]=b;
+        SystemController controller=new SystemController(currency,deno,coindeno,3,3);
+        controller.addInk(controller,1<<20);
+        controller.addPaper(controller,22);
+        rp.printReceipt(controller,true,1,0);
     }
-    @Test
-    public void notifyAttendant_out_of_ink() throws OverloadException {
-
-        Barcode barcode = new Barcode(Numeral.five);
-        String description ="Item";
-        BigDecimal price = new BigDecimal(5);
-        double expected_weight=3;
-        BarcodedProduct bp = new BarcodedProduct(barcode,description,price,expected_weight);
-        ArrayList<BarcodedProduct> items=new ArrayList<BarcodedProduct>();
-        items.add(bp);
-        ReceiptPrinter printer =new ReceiptPrinter();
-        printer.addPaper(55);
-        printer.addInk(1<<20);
-
-        receiptPrinterSoftware rp =new receiptPrinterSoftware(items);
-        receiptPrinterController controller = new receiptPrinterController();
-
-        AttendantObserver observer =new AttendantObserver() {
-            @Override
-            public void notifyAttendant(ReceiptPrinter printer,receiptPrinterController controller, AttendantObserver observer, int paperRemaining, int inkRemaining) throws OverloadException {
-
-            }
-
-            @Override
-            public void putPaper(ReceiptPrinter printer) throws OverloadException {
-
-            }
-
-            @Override
-            public void putInk(ReceiptPrinter printer) throws OverloadException {
-
-            }
-        };
-
-        rp.notifyAttendant(printer,controller,observer,1,0);
-
-    }
-
-    @Test
-    public void putPaper() throws OverloadException {
-
-        Barcode barcode = new Barcode(Numeral.five);
-        String description ="Item";
-        BigDecimal price = new BigDecimal(5);
-        double expected_weight=3;
-        ReceiptPrinter printer=new ReceiptPrinter();
-        printer.addPaper(10);
-        BarcodedProduct bp = new BarcodedProduct(barcode,description,price,expected_weight);
-        ArrayList<BarcodedProduct> items=new ArrayList<BarcodedProduct>();
-        items.add(bp);
-        receiptPrinterSoftware rp =new receiptPrinterSoftware(items);
-        rp.putPaper(printer);
-
-
-    }
-
-    @Test
-    public void putInk() throws OverloadException {
-
-        Barcode barcode = new Barcode(Numeral.five);
-        String description ="Item";
-        BigDecimal price = new BigDecimal(5);
-        double expected_weight=3;
-        ReceiptPrinter printer=new ReceiptPrinter();
-        printer.addPaper(10);
-        BarcodedProduct bp = new BarcodedProduct(barcode,description,price,expected_weight);
-        ArrayList<BarcodedProduct> items=new ArrayList<BarcodedProduct>();
-        items.add(bp);
-        receiptPrinterSoftware rp =new receiptPrinterSoftware(items);
-        rp.putInk(printer);
-
-
-    }
-
 }
