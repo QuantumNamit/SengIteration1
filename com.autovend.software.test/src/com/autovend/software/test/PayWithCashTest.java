bb83d434eea541c8dd5eb52a7e6d66e2d59c8fc5
namitaneja123 <115895907+namitaneja123@users.noreply.github.com>
2023-03-17 23:30:51 -0600

    Update PayWithCashTest.java
    pay with cash test changes

--- com.autovend.software.test/src/com/autovend/software/test/PayWithCashTest.java
+++ com.autovend.software.test/src/com/autovend/software/test/PayWithCashTest.java
@@ -1,99 +1,51 @@
 package com.autovend.software.test;
-import static org.junit.Assert.assertEquals;
+import com.autovend.software.*;
+
+import com.autovend.Barcode;
+import com.autovend.BarcodedUnit;
+import com.autovend.Bill;
+import com.autovend.Numeral;
+import com.autovend.devices.OverloadException;
+import com.autovend.products.BarcodedProduct;
+import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Currency;
-import java.util.Map;
 
-import org.junit.Test;
-
-import com.autovend.*;
-import com.autovend.devices.*;
-import com.autovend.devices.observers.*;
-import com.autovend.external.*;
-import com.autovend.products.*;
-import com.autovend.software.*;
-
-
+import static org.junit.Assert.*;
 
 public class PayWithCashTest {
-	private Currency currency;
-	private int value;
-	// Variable for cash inserted
-	private int[] CashDenominations = {5, 10, 20, 50, 100};
-	private BigDecimal[] coinDenominations;
-	private int  Cash_Inserted;
-	// Variable for total Bill
-	private int Total_Amount;
-	// Variable for total Due
-	private int Amount_Due;
-	// variable for total Amount Paid
-	private int Amount_Paid;
-	// Variable for Change after bill paid
-	private int Change;
-	// Boolean Variable for payment completion
-	private boolean paidinFull;
-	private int scaleMaximumWeight;
-	private int scaleSensitivity;
-	private ArrayList<BarcodedProduct> items;
-	
-	/* Bill 
-	Bill bill = new Bill(value, currency);
-	Bill bill_100 = new Bill (100, currency.getInstance("CAD"));
-	Bill bill_50 = new Bill (50, currency.getInstance("CAD"));
-	Bill bill_20 = new Bill (20, currency.getInstance("CAD"));
-	Bill bill_10 = new Bill (10, currency.getInstance("CAD"));
-	Bill bill_5 = new Bill (5, currency.getInstance("CAD"));
-	*/
-	SystemController controller=new SystemController (currency, CashDenominations, coinDenominations, scaleMaximumWeight, scaleSensitivity);
-	
-	Map<Integer, BillDispenser> bill_dispenser = controller.billDispensers;
-	BillSlot bill_input = controller.billInput; 	// input channel
-	BillSlot bill_output = controller.billOutput; 	// output channel
-	BillStorage bill_storage = controller.billStorage;
-	BillValidator billValidator = controller.billValidator;
+
+    @Test
+    public void cash_Algorithm() throws OverloadException {
+
+        Barcode barcode = new Barcode(Numeral.five);
+        String description ="Item";
+        BigDecimal price = new BigDecimal(5);
+        double expected_weight=3;
+        BarcodedProduct bp = new BarcodedProduct(barcode,description,price,expected_weight);
+
+        ArrayList<BarcodedProduct> items=new ArrayList<BarcodedProduct>();
+        items.add(bp);
 
 
-	/** Tests for if bill value is inserted properly **/
-	@Test
-	public void Check_CashInserted() {
-		PayWithCash Test1= new PayWithCash();
-		Bill bill_20 = new Bill(20, Currency.getInstance("CAD"));
-        int value = Test1.Getting_Bill_Value(bill_20);
-        assertEquals(20, value);
-
-	}
-	/** Tests if the Amount is less than total **/
-	@Test
-	public void Check_CashAlgorithm_AmountLess_thanTotal() {
-		int Cash_inserted= 20;
-		int Total_Bill=55;
-		PayWithCash Test2= new PayWithCash(Cash_inserted, Total_Bill);
-		int Amount_Due= Test2.getAmount_Due();
-		assertEquals(35, Amount_Due);
-	
-	}
-	/** Tests if the Amount is greater than total **/
-	@Test 
-	public void Check_CashAlgorithm_AmountGreater_thanTotal() {
-		int Cash_inserted= 50;
-		int Total_Bill=24;
-		PayWithCash Test2= new PayWithCash(Cash_inserted, Total_Bill);
-		int Change_Due= Test2.getChange();
-		assertEquals(26, Change_Due);
-		
-	}
-	
-	}
+        receiptPrinterSoftware rp =new receiptPrinterSoftware(items);
 
 
+        BarcodedUnit unit=new BarcodedUnit(barcode,1.2);
+        Currency currency = Currency.getInstance("USD");
+        int [] deno = {1,2};
+        BigDecimal[] coindeno=new BigDecimal[2];
+        BigDecimal a =new BigDecimal(1);
+        BigDecimal b =new BigDecimal(2);
+        coindeno[0]=a;
+        coindeno[1]=b;
 
-
-
-
-
-
-
-
-
+        SystemController controller=new SystemController(currency,deno,coindeno,3,3);
+        Bill bill = new Bill(5,currency);
+        PayWithCash obj=new PayWithCash(currency);
+        BigDecimal cashInserted =BigDecimal.valueOf(50);
+        obj.Cash_Algorithm(controller,bp,bill,false,cashInserted);
+    }
+}
